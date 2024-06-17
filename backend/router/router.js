f337e92def5611038ea05005cf1305fc50cb2cb6
Nagendra-Yadav1 <147228690+Nagendra-Yadav1@users.noreply.github.com>
2024-06-17 11:21:28 +0530

    Delete backend directory

--- backend/router/router.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { signup } from '../controller/controller.js';
-
-const router = express.Router();
-
-router.post('/post', signup);
-
-export default router;  
